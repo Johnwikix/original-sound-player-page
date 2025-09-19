837d9c5abdac8e1526014c66cde045c9c7fc2882
pkc <906847109@qq.com>
2025-09-19 13:47:17 +0800

    update

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,21 +6,22 @@ import zh from './locales/zh.json';
 
 // 获取浏览器语言
 const getBrowserLanguage = () => {
-  const lang = navigator.language.toLowerCase();
-  if (lang.startsWith('zh')) {
-    return 'zh';
-  }
-  return 'en';
+    const lang = navigator.language.toLowerCase();
+    if (lang.startsWith('zh')) {
+        return 'zh';
+    }
+    return 'en';
 };
 
 const i18n = createI18n({
-  legacy: false, // 使用 Composition API 模式
-  locale: getBrowserLanguage(), // 设置默认语言为浏览器语言
-  fallbackLocale: 'en', // 当浏览器语言文件不存在时，使用英文作为备选
-  messages: {
-    en,
-    zh
-  }
+    legacy: false, // 使用 Composition API 模式
+    //   locale: getBrowserLanguage(), // 设置默认语言为浏览器语言
+    locale: 'zh',
+    fallbackLocale: 'en', // 当浏览器语言文件不存在时，使用英文作为备选
+    messages: {
+        en,
+        zh
+    }
 });
 
 export default i18n; 
