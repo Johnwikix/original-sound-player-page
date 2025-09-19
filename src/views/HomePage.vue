3152455102884651cfea09c2891da03e46dfcabb
pkc <906847109@qq.com>
2025-09-19 12:48:09 +0800

    内容更新

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,8 +8,6 @@
                     <div><img src="../assets/bg3.png" alt="背景图3" class="carousel-image"></div>
                 </a-carousel>
 
-                <!-- <div class="hero-overlay"></div> -->
-
                 <div class="hero-content">
                     <a-typography-title class="hero-title">
                         原音HQ播放器
@@ -33,11 +31,11 @@
 
             <section class="features-section">
                 <a-row :gutter="[32, 32]" justify="center">
-                    <a-col :xs="24" :md="12" :lg="8">
+                    <a-col :xs="24" :md="12" :lg="6">
                         <a-card class="feature-card">
                             <a-typography-title class="feature-card-title" :level="3">沉浸式的现代界面</a-typography-title>
                             <a-typography-paragraph class="feature-card-content">
-                                精心设计的用户界面，提供流畅、直观的交互体验。支持多种亚克力效果等多项自定义设置，打造你专属的音乐播放器。
+                                精心设计的用户界面，提供流畅、直观的交互体验。支持多种亚克力效果，让你的音乐之旅赏心悦目。多项自定义设置，打造你专属的音乐播放器。
                             </a-typography-paragraph>
                             <div class="feature-image-wrapper">
                                 <a-image :src="playing" alt="个性化主题" class="feature-image" />
@@ -45,11 +43,11 @@
                         </a-card>
                     </a-col>
 
-                    <a-col :xs="24" :md="12" :lg="8">
+                    <a-col :xs="24" :md="12" :lg="6">
                         <a-card class="feature-card">
                             <a-typography-title class="feature-card-title" :level="3">掌控你的音乐库</a-typography-title>
                             <a-typography-paragraph class="feature-card-content">
-                                自动扫描并管理你的本地音乐文件。轻松按专辑、艺术家或文件夹浏览，还能进行元数据编辑，自动匹配歌词和歌曲封面。
+                                自动扫描并管理你的本地音乐文件。轻松按专辑、艺术家或文件夹浏览，支持创建导入导出自定义播放列表，还能进行元数据编辑，自动匹配歌词和歌曲封面。
                             </a-typography-paragraph>
                             <div class="feature-image-wrapper">
                                 <a-image :src="album" alt="音乐库管理" class="feature-image" />
@@ -57,23 +55,23 @@
                         </a-card>
                     </a-col>
 
-                    <a-col :xs="24" :md="12" :lg="8">
+                    <a-col :xs="24" :md="12" :lg="6">
                         <a-card class="feature-card">
                             <a-typography-title class="feature-card-title" :level="3">极致音质体验</a-typography-title>
                             <a-typography-paragraph class="feature-card-content">
-                                支持包括flac,wav,mp3,dsf,ape,m4a,wmv,aiff,oga等多种音频格式，搭配均衡器和wasapi独占模式，还原音乐最本真的细节。
+                                支持包括 flac,wav,mp3,dsf,ape,m4a,wmv,aiff,oga 等多种音频格式，搭配均衡器和wasapi独占模式，还原音乐最本真的细节。
                             </a-typography-paragraph>
                             <div class="feature-image-wrapper">
                                 <a-image :src="wasapiImage" alt="极致音质体验" class="feature-image" />
                             </div>
                         </a-card>
                     </a-col>
-                    <a-col :xs="24" :md="12" :lg="8">
+                    <a-col :xs="24" :md="12" :lg="6">
                         <a-card class="feature-card">
                             <a-typography-title class="feature-card-title" :level="3">Walkman 伴侣</a-typography-title>
                             <a-typography-paragraph class="feature-card-content">
-                                轻松将你的音乐转换为 **WAV、MP3、FLAC** 等格式，并方便地通过 USB 传输到你的 Walkman
-                                上。软件还能自动同步已匹配好的**元信息和歌词文件**，让你的随身听体验更上一层楼。
+                                轻松将你的音乐转换为 WAV、MP3、FLAC 等格式，并方便地通过 USB 连同同步好的歌词文件一同传输到你的 Walkman
+                                上，让你的随身听体验更上一层楼。
                             </a-typography-paragraph>
                             <div class="feature-image-wrapper">
                                 <a-image :src="walkman" alt="Walkman 伴侣" class="feature-image" />
@@ -84,15 +82,30 @@
             </section>
 
             <section class="download-section">
-                <a-typography-title class="feature-card-title" :level="2">
-                    音频可视化
-                </a-typography-title>
-                <a href="https://apps.microsoft.com/store/detail/9PL2DSHJ79W7?referrer=appbadge&amp;mode=direct"
-                    rel="nofollow">
-                    <img src="https://camo.githubusercontent.com/16f54569169711557fd6d3833dfaea3239506d7f8d673b07573c52855f1f2ee4/68747470733a2f2f6765742e6d6963726f736f66742e636f6d2f696d616765732f656e2d75732532306461726b2e737667"
-                        width="200" data-canonical-src="https://get.microsoft.com/images/en-us%20dark.svg"
-                        style="max-width: 100%;">
-                </a>
+                <a-row :gutter="[48, 48]" align="middle">
+                    <a-col :xs="24" :md="12">
+                        <div class="visualization-content">
+                            <a-typography-title class="feature-card-title" :level="2">
+                                音频可视化
+                            </a-typography-title>
+                            <a-typography-paragraph class="feature-card-content"
+                                style="font-size: 1.1vw; line-height: 1.6; margin: 24px 0;">
+                                频谱分析与丰富的动态视觉效果完美融合。支持实时音频数据处理,让音乐不仅仅是听觉的盛宴，更是视觉的艺术体验。
+                            </a-typography-paragraph>
+                            <a href="https://apps.microsoft.com/store/detail/9PL2DSHJ79W7?referrer=appbadge&amp;mode=direct"
+                                rel="nofollow">
+                                <img src="https://camo.githubusercontent.com/16f54569169711557fd6d3833dfaea3239506d7f8d673b07573c52855f1f2ee4/68747470733a2f2f6765742e6d6963726f736f66742e636f6d2f696d616765732f656e2d75732532306461726b2e737667"
+                                    width="200" data-canonical-src="https://get.microsoft.com/images/en-us%20dark.svg"
+                                    style="max-width: 100%;">
+                            </a>
+                        </div>
+                    </a-col>
+                    <a-col :xs="24" :md="12">
+                        <div class="visualization-image-wrapper">
+                            <a-image :src="visualizationImage" alt="音频可视化界面" class="visualization-image" />
+                        </div>
+                    </a-col>
+                </a-row>
             </section>
         </a-layout-content>
 
@@ -119,6 +132,7 @@ import wasapiImage from '../assets/wasapi.jpg';
 import playing from '../assets/playing.jpg';
 import album from '../assets/album.jpg';
 import walkman from '../assets/album.jpg';
+import visualizationImage from '../assets/visualization.png';
 </script>
 
 <style scoped>
@@ -162,7 +176,7 @@ import walkman from '../assets/album.jpg';
 
 .hero-video {
     border-radius: 10px;
-    height: 80%;
+    height: 85%;
 }
 
 /* 走马灯样式 */
@@ -276,7 +290,7 @@ import walkman from '../assets/album.jpg';
 
 /* 下载区域样式 */
 .download-section {
-    padding: 60px 24px;
+    padding: 0vh 6vw;
     text-align: center;
 }
 
@@ -287,6 +301,14 @@ import walkman from '../assets/album.jpg';
     padding: 24px;
     color: #c9d1d9;
     border-top: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+.visualization-image-wrapper {
+    margin: 4vw;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 /* 响应式设计 */
